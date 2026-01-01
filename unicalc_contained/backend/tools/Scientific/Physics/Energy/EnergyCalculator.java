13df3c78c10b7abe3f203bb71e778ce723427177
Fredrik Hellevang <fredrik.marthin.hellevang@nmbu.no>
2026-01-01 17:56:48 +0100

    name test

--- unicalc_contained/backend/tools/Scientific/Physics/Energy/EnergyCalculator.java
+++ unicalc_contained/backend/tools/Scientific/Physics/Energy/EnergyCalculator.java
@@ -1,5 +1,5 @@
 package unicalc_contained.backend.tools.Physics.Energy;
-public class Energy_Calculator {
+public class EnergyCalculator {
     public static void main(String[] args) {
         double mass = Double.parseDouble(args[0]);
         double c = 299_792_458;
