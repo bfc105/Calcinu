fb6ddf1389441d3be28f6d96b3e613df09dad7c2
Fredrik Hellevang <fredrik.marthin.hellevang@nmbu.no>
2026-01-01 16:31:10 +0100

    debug print statements for frontend

--- unicalc_contained/frontend/src/components/Energy_Calculator.tsx
+++ unicalc_contained/frontend/src/components/Energy_Calculator.tsx
@@ -17,6 +17,14 @@ const EnergyCalculator: React.FC<ToolProps> = ({
     setError(null);
     setEnergy(null);
 
+    console.log({
+      domain,
+      field,
+      topic,
+      tool,
+      input: mass.toString(),
+    });
+
     try {
       const response = await fetch("/api/unordered_one_to_one", {
         method: "POST",
